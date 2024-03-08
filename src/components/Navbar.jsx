f367e5ebf326cef7b15e8ce0ea76c410d0c773dc
Fernando <codeferlu@gmail.com>
2024-03-08 15:42:31 -0300

    arreglo navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,11 +5,11 @@ import Container from 'react-bootstrap/Container';
 const Header = (props) => {
     const setActiveClass = ({isActive}) => isActive ? 'active' : ''
     return (
-        <Navbar bg="dark" data-bs-theme="dark">
+        <Navbar className="navbar" bg="dark" data-bs-theme="dark">
              <Container>
                 <NavLink className={setActiveClass} to="/" >Who i'm i</NavLink>
                 <NavLink className={setActiveClass} to="/projects" >Proyects</NavLink>
-                <NavLink className={setActiveClass} to="/work" >Work with me</NavLink>
+                <NavLink className={setActiveClass} to="/work" >Lets Work Together!</NavLink>
              </Container>
         </Navbar>
     )
